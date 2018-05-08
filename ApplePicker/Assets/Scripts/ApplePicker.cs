d361eb1ad240765f02013620f241c044e8c4a471
H3R5H1 <31782381+H3R5H1@users.noreply.github.com>
2018-05-07 20:51:07 -0400

    gamedesign applepicker
    gamedesign apple picker

---
+++ ApplePicker/Assets/Scripts/ApplePicker.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class ApplePicker : MonoBehaviour {
+
+	public GameObject basketPrefab;
+	public List<GameObject> basketList;
+	public int numBaskets = 3;
+	public float basketBottomY = -14f;
+	public float basketSpacingY = 2f;
+
+	
+	void Start () {
+		basketList = new List<GameObject> ();
+		for (int i = 0; i < numBaskets; i++) {
+			
+			GameObject tBasketGo = Instantiate (basketPrefab) as GameObject;
+			Vector3 pos = Vector3.zero;
+			pos.y = basketBottomY + (basketSpacingY * i);
+			tBasketGo.transform.position = pos;
+			basketList.Add (tBasketGo);
+		}
+	}
+
+	public void AppleDestroyed(){
+		
+		GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag ("Apple");
+		foreach(GameObject tGo in tAppleArray){
+				Destroy (tGo);
+		}
+
+		//basket function
+		int basketIndex = basketList.Count - 1;
+		GameObject tBasketGO = basketList [basketIndex];
+		basketList.RemoveAt (basketIndex); 
+		Destroy (tBasketGO); 
+
+        if(basketList.Count == 0)
+        {
+            Application.LoadLevel("_Scene_0");
+        }
+	}
+}
